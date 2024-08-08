e43c16f9175a0a72d70b613c01649629756d7c45
Fam Hesby Ahlander <famhesbyahlander@erik-sin-imac.lan>
2024-08-08 15:31:51 +0200

    structure

---
+++ script.js
@@ -0,0 +1,17 @@
+const categories = [
+    {
+        name: "sport",
+        questions: [
+            {
+
+            }
+        ]
+    }
+]
+
+const button1 = document.getElementById("option1");
+
+
+function inGame(){
+    button1.onclick = //highlight button1
+}
